bfa9d66c152760d21cf80432a4e756d88d0fb71b
Jianfeng Zhu <jianfengzhu@microsoft.com>
2022-08-13 16:49:07 +0800

    init

---
+++ option7.js
@@ -0,0 +1,89 @@
+option71 = {
+
+    title: {
+        //text: `${value}万辆`,
+        text: `其他0.1%`,
+        subtext: '',
+        left: 'center',
+        top: 'center',//top待调整
+        textStyle: {
+            color: '#fff',
+            fontSize: 16,
+            fontFamily: 'PingFangSC-Regular'
+        },
+        subtextStyle: {
+            color: '#ff',
+            fontSize: 14,
+            fontFamily: 'PingFangSC-Regular',
+            top: 'center'
+        },
+        itemGap: -1//主副标题间距
+    },
+
+    series: [{
+        name: 'pie1',
+        type: 'pie',
+        clockWise: true,
+        radius: ['65%', '70%'],
+        itemStyle: {
+            normal: {
+                label: {
+                    show: false
+                },
+                labelLine: {
+                    show: false
+                }
+            }
+        },
+        hoverAnimation: false,
+        data: [{
+            value: value,
+            name: 'completed',
+            itemStyle: {
+                normal: {
+                    borderWidth: 8,
+                    borderColor: {
+                        colorStops: [{
+                            offset: 0,
+                            color: '#eb3600' || '#cc9a00' // 0% 处的颜色
+                        }, {
+                            offset: 1,
+                            color: '#d0a00e' || '#d0570e' // 100% 处的颜色
+                        }]
+                    },
+                    color: { // 完成的圆环的颜色
+                        colorStops: [{
+                            offset: 0,
+                            color: '#eb3600' || '#cc9a00' // 0% 处的颜色
+                        }, {
+                            offset: 1,
+                            color: '#d0a00e' || '#d0570e' // 100% 处的颜色
+                        }]
+                    },
+                    label: {
+                        show: false
+                    },
+                    labelLine: {
+                        show: false
+                    }
+                }
+            }
+        }, {
+            name: 'gap',
+            value: 100 - value,
+            itemStyle: {
+                normal: {
+                    label: {
+                        show: false
+                    },
+                    labelLine: {
+                        show: false
+                    },
+                    color: 'rgba(0, 0, 0, 0)',
+                    borderColor: 'rgba(0, 0, 0, 0)',
+                    borderWidth: 0
+                }
+            }
+        }]
+    }]
+}
